1dd220dbb82856e82b1377eaf5458321470f6950
lostcache <harshdevisha9111@gmail.com>
2025-08-05 15:40:25 +0000

    minimal kernel

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -xue
+
+# QEMU file path
+QEMU=qemu-system-riscv32
+
+# clang and compiler flags
+CC=clang
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fno-stack-protector -ffreestanding -nostdlib"
+
+# Building the Kernel
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c
+
+# Start QEMU
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot
