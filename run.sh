ac34fdeb672e3ddc11105ea09673fa802f6aa4db
lostcache <harshdevisha9111@gmail.com>
2025-08-07 14:17:02 +0000

    hello world

--- run.sh
+++ run.sh
@@ -12,4 +12,5 @@ CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fno-stack-p
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c
 
 # Start QEMU
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel kernel.elf
+
