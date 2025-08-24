d4b869cb64225c8ef539a4af8b45c05a1610129c
lostcache <harshdevisha9111@gmail.com>
2025-08-24 03:40:03 +0000

    fixes function signature for strcpy

--- common.h
+++ common.h
@@ -20,6 +20,6 @@ typedef uint32_t vaddr_t;
 
 void *memset(void *buf, char c, size_t n);
 void *memcpy(void *dst, const void *src, size_t n);
-void *strcpy(char *dst, const char *src);
+char *strcpy(char *dst, const char *src);
 int strcmp(const char *s1, const char *s2);
 void printf(const char *fmt, ...);
