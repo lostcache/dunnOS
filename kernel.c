dae85f80fe565cedc83cdf4c456c9d2f26713a73
lostcache <harshdevisha9111@gmail.com>
2025-09-05 22:00:01 +0000

    ram and basic mem allocator

--- kernel.c
+++ kernel.c
@@ -6,6 +6,20 @@ typedef unsigned int uint32_t;
 typedef uint32_t size_t;
 
 extern char __bss[], __bss_end[], __stack_top[];
+extern char __free_ram[], __free_ram_end[];
+
+paddr_t alloc_pages(uint32_t n) {
+    static paddr_t next_paddr = (paddr_t)__free_ram;
+    paddr_t paddr = next_paddr;
+    next_paddr += n * PAGE_SIZE;
+
+    if (next_paddr > (paddr_t)__free_ram_end) {
+        PANIC("out of memory");
+    }
+
+    memset((void *)paddr, 0, n * PAGE_SIZE);
+    return paddr;
+}
 
 struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4,
                        long arg5, long fid, long eid) {
@@ -114,13 +128,20 @@ void handle_trap(struct trap_frame *f) {
 void kernel_main(void) {
     memset(__bss, 0, (size_t)__bss_end - (size_t)__bss);
 
-    WRITE_CSR(stvec, (uint32_t)kernel_entry);
-    __asm__ __volatile__("unimp");
+    // WRITE_CSR(stvec, (uint32_t)kernel_entry);
+    // __asm__ __volatile__("unimp");
 
-    printf("\n\nHello %s\n", "World!");
-    for (;;) {
-        __asm__ __volatile__("wfi");
-    }
+    // printf("\n\nHello %s\n", "World!");
+    // for (;;) {
+    //     __asm__ __volatile__("wfi");
+    // }
+
+    paddr_t paddr0 = alloc_pages(2);
+    paddr_t paddr1 = alloc_pages(1);
+    printf("alloc_pages test: paddr=%x\n", paddr0);
+    printf("alloc_pages test: paddr1=%x\n", paddr1);
+
+    PANIC("booted!");
 }
 
 __attribute__((section(".text.boot"))) __attribute__((naked)) void boot(void) {
