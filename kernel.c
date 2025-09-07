4ade105df78c106add9d5cf02c0bb239ed381089
lostcache <harshdevisha9111@gmail.com>
2025-09-07 00:13:48 +0000

    basic RR scheduler

--- kernel.c
+++ kernel.c
@@ -125,77 +125,85 @@ struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4,
 void putchar(char ch) { sbi_call(ch, 0, 0, 0, 0, 0, 0, 1); }
 
 __attribute__((naked)) __attribute__((aligned(4))) void kernel_entry(void) {
-    __asm__ __volatile__("csrw sscratch, sp\n"
-                         "addi sp, sp, -4 * 31\n"
-                         "sw ra,  4 * 0(sp)\n"
-                         "sw gp,  4 * 1(sp)\n"
-                         "sw tp,  4 * 2(sp)\n"
-                         "sw t0,  4 * 3(sp)\n"
-                         "sw t1,  4 * 4(sp)\n"
-                         "sw t2,  4 * 5(sp)\n"
-                         "sw t3,  4 * 6(sp)\n"
-                         "sw t4,  4 * 7(sp)\n"
-                         "sw t5,  4 * 8(sp)\n"
-                         "sw t6,  4 * 9(sp)\n"
-                         "sw a0,  4 * 10(sp)\n"
-                         "sw a1,  4 * 11(sp)\n"
-                         "sw a2,  4 * 12(sp)\n"
-                         "sw a3,  4 * 13(sp)\n"
-                         "sw a4,  4 * 14(sp)\n"
-                         "sw a5,  4 * 15(sp)\n"
-                         "sw a6,  4 * 16(sp)\n"
-                         "sw a7,  4 * 17(sp)\n"
-                         "sw s0,  4 * 18(sp)\n"
-                         "sw s1,  4 * 19(sp)\n"
-                         "sw s2,  4 * 20(sp)\n"
-                         "sw s3,  4 * 21(sp)\n"
-                         "sw s4,  4 * 22(sp)\n"
-                         "sw s5,  4 * 23(sp)\n"
-                         "sw s6,  4 * 24(sp)\n"
-                         "sw s7,  4 * 25(sp)\n"
-                         "sw s8,  4 * 26(sp)\n"
-                         "sw s9,  4 * 27(sp)\n"
-                         "sw s10, 4 * 28(sp)\n"
-                         "sw s11, 4 * 29(sp)\n"
+    __asm__ __volatile__(
+        // Retrieve the kernel stack of the running process from sscratch.
+        "csrrw sp, sscratch, sp\n"
 
-                         "csrr a0, sscratch\n"
-                         "sw a0, 4 * 30(sp)\n"
+        "addi sp, sp, -4 * 31\n"
+        "sw ra,  4 * 0(sp)\n"
+        "sw gp,  4 * 1(sp)\n"
+        "sw tp,  4 * 2(sp)\n"
+        "sw t0,  4 * 3(sp)\n"
+        "sw t1,  4 * 4(sp)\n"
+        "sw t2,  4 * 5(sp)\n"
+        "sw t3,  4 * 6(sp)\n"
+        "sw t4,  4 * 7(sp)\n"
+        "sw t5,  4 * 8(sp)\n"
+        "sw t6,  4 * 9(sp)\n"
+        "sw a0,  4 * 10(sp)\n"
+        "sw a1,  4 * 11(sp)\n"
+        "sw a2,  4 * 12(sp)\n"
+        "sw a3,  4 * 13(sp)\n"
+        "sw a4,  4 * 14(sp)\n"
+        "sw a5,  4 * 15(sp)\n"
+        "sw a6,  4 * 16(sp)\n"
+        "sw a7,  4 * 17(sp)\n"
+        "sw s0,  4 * 18(sp)\n"
+        "sw s1,  4 * 19(sp)\n"
+        "sw s2,  4 * 20(sp)\n"
+        "sw s3,  4 * 21(sp)\n"
+        "sw s4,  4 * 22(sp)\n"
+        "sw s5,  4 * 23(sp)\n"
+        "sw s6,  4 * 24(sp)\n"
+        "sw s7,  4 * 25(sp)\n"
+        "sw s8,  4 * 26(sp)\n"
+        "sw s9,  4 * 27(sp)\n"
+        "sw s10, 4 * 28(sp)\n"
+        "sw s11, 4 * 29(sp)\n"
 
-                         "mv a0, sp\n"
-                         "call handle_trap\n"
+        // Retrieve and save the sp at the time of exception.
+        "csrr a0, sscratch\n"
+        "sw a0, 4 * 30(sp)\n"
 
-                         "lw ra,  4 * 0(sp)\n"
-                         "lw gp,  4 * 1(sp)\n"
-                         "lw tp,  4 * 2(sp)\n"
-                         "lw t0,  4 * 3(sp)\n"
-                         "lw t1,  4 * 4(sp)\n"
-                         "lw t2,  4 * 5(sp)\n"
-                         "lw t3,  4 * 6(sp)\n"
-                         "lw t4,  4 * 7(sp)\n"
-                         "lw t5,  4 * 8(sp)\n"
-                         "lw t6,  4 * 9(sp)\n"
-                         "lw a0,  4 * 10(sp)\n"
-                         "lw a1,  4 * 11(sp)\n"
-                         "lw a2,  4 * 12(sp)\n"
-                         "lw a3,  4 * 13(sp)\n"
-                         "lw a4,  4 * 14(sp)\n"
-                         "lw a5,  4 * 15(sp)\n"
-                         "lw a6,  4 * 16(sp)\n"
-                         "lw a7,  4 * 17(sp)\n"
-                         "lw s0,  4 * 18(sp)\n"
-                         "lw s1,  4 * 19(sp)\n"
-                         "lw s2,  4 * 20(sp)\n"
-                         "lw s3,  4 * 21(sp)\n"
-                         "lw s4,  4 * 22(sp)\n"
-                         "lw s5,  4 * 23(sp)\n"
-                         "lw s6,  4 * 24(sp)\n"
-                         "lw s7,  4 * 25(sp)\n"
-                         "lw s8,  4 * 26(sp)\n"
-                         "lw s9,  4 * 27(sp)\n"
-                         "lw s10, 4 * 28(sp)\n"
-                         "lw s11, 4 * 29(sp)\n"
-                         "lw sp,  4 * 30(sp)\n"
-                         "sret\n");
+        // Reset the kernel stack.
+        "addi a0, sp, 4 * 31\n"
+        "csrw sscratch, a0\n"
+
+        "mv a0, sp\n"
+        "call handle_trap\n"
+
+        "lw ra,  4 * 0(sp)\n"
+        "lw gp,  4 * 1(sp)\n"
+        "lw tp,  4 * 2(sp)\n"
+        "lw t0,  4 * 3(sp)\n"
+        "lw t1,  4 * 4(sp)\n"
+        "lw t2,  4 * 5(sp)\n"
+        "lw t3,  4 * 6(sp)\n"
+        "lw t4,  4 * 7(sp)\n"
+        "lw t5,  4 * 8(sp)\n"
+        "lw t6,  4 * 9(sp)\n"
+        "lw a0,  4 * 10(sp)\n"
+        "lw a1,  4 * 11(sp)\n"
+        "lw a2,  4 * 12(sp)\n"
+        "lw a3,  4 * 13(sp)\n"
+        "lw a4,  4 * 14(sp)\n"
+        "lw a5,  4 * 15(sp)\n"
+        "lw a6,  4 * 16(sp)\n"
+        "lw a7,  4 * 17(sp)\n"
+        "lw s0,  4 * 18(sp)\n"
+        "lw s1,  4 * 19(sp)\n"
+        "lw s2,  4 * 20(sp)\n"
+        "lw s3,  4 * 21(sp)\n"
+        "lw s4,  4 * 22(sp)\n"
+        "lw s5,  4 * 23(sp)\n"
+        "lw s6,  4 * 24(sp)\n"
+        "lw s7,  4 * 25(sp)\n"
+        "lw s8,  4 * 26(sp)\n"
+        "lw s9,  4 * 27(sp)\n"
+        "lw s10, 4 * 28(sp)\n"
+        "lw s11, 4 * 29(sp)\n"
+        "lw sp,  4 * 30(sp)\n"
+        "sret\n");
 }
 
 void handle_trap(struct trap_frame *f) {
@@ -213,6 +221,32 @@ void delay(void) {
     }
 }
 
+struct process *curr_proc;
+struct process *idle_proc;
+
+void yield(void) {
+    struct process *next = idle_proc;
+    for (int i = 0; i < PROCS_MAX; i++) {
+        struct process *proc = &procs[(curr_proc->pid + i) % PROCS_MAX];
+        if (proc->state == PROC_RUNNABLE && proc->pid > 0) {
+            next = proc;
+            break;
+        }
+    }
+
+    if (next == curr_proc)
+        return;
+
+    __asm__ __volatile__(
+        "csrw sscratch, %[sscratch]\n"
+        :
+        : [sscratch] "r"((uint32_t)&next->stack[sizeof(next->stack)]));
+
+    struct process *prev = curr_proc;
+    curr_proc = next;
+    switch_context(&prev->sp, &next->sp);
+}
+
 struct process *proc_a;
 struct process *proc_b;
 
@@ -220,8 +254,7 @@ void proc_a_entry(void) {
     printf("starting process A\n");
     while (1) {
         putchar('A');
-        switch_context(&proc_a->sp, &proc_b->sp);
-        delay();
+        yield();
     }
 }
 
@@ -229,8 +262,7 @@ void proc_b_entry(void) {
     printf("starting process B\n");
     while (1) {
         putchar('B');
-        switch_context(&proc_b->sp, &proc_a->sp);
-        delay();
+        yield();
     }
 }
 
@@ -250,10 +282,14 @@ void kernel_main(void) {
     // printf("alloc_pages test: paddr=%x\n", paddr0);
     // printf("alloc_pages test: paddr1=%x\n", paddr1);
 
+    idle_proc = create_process((uint32_t)NULL);
+    idle_proc->pid = 0;
+    curr_proc = idle_proc;
     proc_a = create_process((uint32_t)proc_a_entry);
     proc_b = create_process((uint32_t)proc_b_entry);
+    yield();
     // proc_a_entry();
-    PANIC("unreachable here!");
+    PANIC("switched to idle process");
 }
 
 __attribute__((section(".text.boot"))) __attribute__((naked)) void boot(void) {
